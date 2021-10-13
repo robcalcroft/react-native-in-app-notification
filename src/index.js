3c022c9d64ba5284a099374b0acda17a9c637e09
Darien Lombardi <darienlombardi@Dariens-MacBook-Pro.local>
2021-10-13 11:45:06 -0400

    expose context for the usage of hooks by lib consumer

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 export { default as Notification } from './Notification';
-
+export { default as Context } from './Context';
 export { default as InAppNotificationProvider } from './Provider';
 export { default as withInAppNotification } from './WithInAppNotification.hoc';
